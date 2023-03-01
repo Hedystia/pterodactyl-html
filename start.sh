7ec8ed6f391189565fcb7cca97cc898537eb349b
Zastinian <zastinian@gmail.com>
2023-03-01 01:15:08 -0600

    feat: Code

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+
+echo "Starting PHP-FPM..."
+PHP_FPM=$(find /usr/sbin -name "php-fpm*" -type f | tail -n 1)
+$PHP_FPM --fpm-config /home/container/php-fpm/php-fpm.conf -c /home/container/php-fpm/ --daemonize
+
+echo "Starting Html..."
+./html-server run --watch --config ./html/Htmlfile
