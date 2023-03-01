7d0dcee684899de08e19edee89537f4e873a04cb
Zastinian <zastinian@gmail.com>
2023-03-01 01:48:51 -0600

    feat: Code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pterodactyl-html
+
+## Links
+
+> - [Egg](https://github.com/Zastinian/eseggs/tree/master/html)
+> - [Website](https://mresmile.com/)
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-
-echo "Starting PHP-FPM..."
-PHP_FPM=$(find /usr/sbin -name "php-fpm*" -type f | tail -n 1)
-$PHP_FPM --fpm-config /home/container/php-fpm/php-fpm.conf -c /home/container/php-fpm/ --daemonize
-
-echo "Starting Html..."
-./html-server run --watch --config ./html/Htmlfile
